65453d2cfb4a7193e46438c57a8455b0ed9b554f
Eman <emanabukhater2001@gmail.com>
2025-03-09 21:12:00 +0300

    Added weather icons based on weather conditions

--- script.js
+++ script.js
@@ -16,6 +16,28 @@ async function checkWeather(city) {
         document.querySelector('.city').innerText = data.name;
         document.querySelector('.wind').innerText = data.wind.speed + "km/h";
 
+        if (data.weather[0].main === "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+
+        } else if (data.weather[0].main === "Rain") {
+            weatherIcon.src = "images/rain.png";
+        }
+        else if (data.weather[0].main === "Clear") {
+            weatherIcon.src = "images/clear.png";
+        }
+        else if (data.weather[0].main === "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+
+        }
+        else if (data.weather[0].main === "Mist") {
+            weatherIcon.src = "images/mist.png";
+        }
+
+
+
+    
+
+
 
 }
 
